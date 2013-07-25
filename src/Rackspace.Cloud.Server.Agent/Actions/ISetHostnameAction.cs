33703a2777f1648e25b5d711fd53760375e9f227
Kashi Reddy <kashivreddy@gmail.com>
2013-07-25 12:28:12 +0530

    Added sethostname command. This is required as PV tools 6.1 doesn't support renaming host. https://bugs.launchpad.net/nova/+bug/1182934

---
+++ src/Rackspace.Cloud.Server.Agent/Actions/ISetHostnameAction.cs
@@ -0,0 +1,7 @@
+namespace Rackspace.Cloud.Server.Agent.Actions
+{
+    public interface ISetHostnameAction
+    {
+        string SetHostname(string hostname);
+    }
+}
---
+++ src/Rackspace.Cloud.Server.Agent/Actions/SetHostnameAction.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Management;
+
+namespace Rackspace.Cloud.Server.Agent.Actions
+{
+    public class SetHostnameAction : ISetHostnameAction
+    {
+        public string SetHostname(string hostname)
+        {
+            var oldName = Environment.MachineName;
+            using (var cs = new ManagementObject(@"Win32_Computersystem.Name='" + oldName + "'"))
+            {
+                cs.Get();
+                var inParams = cs.GetMethodParameters("Rename");
+                inParams.SetPropertyValue("Name", hostname);
+                var methodOptions = new InvokeMethodOptions(null, TimeSpan.MaxValue);
+                var outParams = cs.InvokeMethod("Rename", inParams, methodOptions);
+                return Convert.ToString(outParams.Properties["ReturnValue"].Value);
+            }
+        }
+    }
+}
